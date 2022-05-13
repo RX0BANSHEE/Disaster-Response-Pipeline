8994d39c752aa57e8648dee329f80959e1c29f58
Bing <li.bing@northeastern.edu>
2022-05-13 16:53:15 -0400

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,70 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """Load and Merge messages and categories data
+    
+    Arguments:
+        messages_filepath {str} -- CSV filepath of message data.
+        categories_filepath {str} -- CSV filepath of categories data.
+    
+    Returns:
+        Dataframe -- Pandas Dataframe of mereged data
+    """
+    # Load data
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+
+    # Merge data
+    df = messages.merge(categories, how='outer',on='id')
+    
+    
+    return df
 
 
 def clean_data(df):
-    pass
+    """Feature engineering and Data cleaning.
+    1. Prepare target Data for multi-output classifier
+    2. Fix data type
+    3. Drop duplicates and unnecessary data
+    
+    Arguments:
+        df {Dateframe} -- Pandas Dataframe of mereged data
+    
+    Returns:
+        df -- Pandas Dataframe of processed data
+    """
+    # Prepare target data for multi-output classifier
+    categories = df['categories'].str.split(';', expand = True)
+    row = categories.iloc[0,:]
+    category_colnames = row.transform(lambda x: x[:-2]).tolist()
+    categories.columns = category_colnames
+    for column in categories:
+        categories[column] = categories[column].transform(lambda x: x[-1:])
+        # Fix Data type
+        categories[column] = categories[column].astype(int)
+    df = df.drop('categories',axis=1)
+    df = pd.concat([df, categories], axis = 1)
 
+    # Drop unnecessary data
+    df.drop('child_alone',axis=1, inplace = True)
+    df = df[df['related'] != 2]
+    df = df.drop_duplicates()
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    """Save processed data to a SQLite database
+    
+    Arguments:
+        df {Dataframe} -- Pandas Dataframe of processed data
+        database_filename {str} -- Table name
+    """
+    database_filename
+    engine = create_engine('sqlite:///'+database_filename)
+    # Save data to df table
+    df.to_sql('DisasterResponse', engine,if_exists = 'replace', index=False)  
 
 
 def main():
